1761dd55cab47dd458d86e594b280d8f2a46d308
louashby <shoxx@web.de>
2013-04-20 14:11:23 +0300

    Create FinanceParameterConstant.java

---
+++ FinanceParameterConstant.java
@@ -0,0 +1,11 @@
+public class FinanceParameterConstant {
+  private double cfgTotalPrice;
+
+	public double getCfgTotalPrice() {
+		return cfgTotalPrice;
+	}
+
+	public void setCfgTotalPrice(double cfgTotalPrice) {
+		this.cfgTotalPrice = cfgTotalPrice;
+	}
+}
