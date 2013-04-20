8bca194114f0847debf454bef424cf0514ed8040
louashby <shoxx@web.de>
2013-04-20 14:09:40 +0300

    Create FinanceParameter.java

---
+++ FinanceParameter.java
@@ -0,0 +1,31 @@
+
+
+public class FinanceParameter implements Serializable {
+
+  private static final long serialVersionUID = 481518252371291858L;
+	private String sliderSize = "";
+	private int index;
+	private String fieldType = "";
+	private String minValue = "";
+	private String sliderPosition = "";
+	private String groupCode = "";
+	private String label = "";
+	private String code = "";
+	private String staticTextFlag = "";
+	private String unit;
+	private String dataType = "";
+	private String screenDisplayType = "";
+	private String unitSize = "";
+	private String parameterDescription = "";
+	private String fieldSize = "";
+	private String value = "";
+	private String fieldAlignment = "";
+	private String allowedValues = "";
+	private String unitDisplay = "";
+	private String maxValue = "";
+	private String parameterDisplayPattern;
+	private String unitAlignment = "";
+	private boolean displayed;
+	private String languageCode = "";
+	
+	public FinanceParameter() { }
